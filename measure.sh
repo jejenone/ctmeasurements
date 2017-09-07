1e0fd70cbd7771c4d0ab3a9e93284515db9ac827
Jerome Fleury <jerome@fleury.net>
2017-09-07 01:46:15 +0000

    mtr stderr to stdout

--- measure.sh
+++ measure.sh
@@ -3,7 +3,7 @@
 TODAY=`date +%F`
 NOW=`date +"%F.%T"`
 
-mtr --report-wide -c 20 --show-ips www.cloudflare.com >>mtr_$TODAY
+mtr --report-wide -c 20 --show-ips www.cloudflare.com 2>&1 >>mtr_$TODAY
 
 echo >>mtr_$TODAY
 
