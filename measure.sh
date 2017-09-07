5dd999201beede361a3a15babbaa6c439e837574
Jerome Fleury <jerome@fleury.net>
2017-09-07 01:44:46 +0000

    cosmetic

--- measure.sh
+++ measure.sh
@@ -2,7 +2,9 @@
 
 TODAY=`date +%F`
 NOW=`date +"%F.%T"`
+
 mtr --report-wide -c 20 --show-ips www.cloudflare.com >>mtr_$TODAY
-echo -e "" >>mtr_$TODAY
+
+echo >>mtr_$TODAY
 
 git add -A && git commit -a -m "$NOW add measurement" && git push
